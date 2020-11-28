8ecb2a459df91278aa638b695ca04797c934cebd
rahul <rahul.rsavant@gmail.com>
2020-11-28 14:48:03 +0530

    *ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <h2 *ngIf ="isTrue; else elseBlock">
+    codeevolution
+  </h2>
+
+  <ng-template #elseBlock>
+    <h2>
+      name is hidden
+    </h2>
+  </ng-template>
+  `,
+  styles: ['./app.component.css']
 })
 export class AppComponent {
   title = 'codeevalution';
+  public isTrue=false;
 }
