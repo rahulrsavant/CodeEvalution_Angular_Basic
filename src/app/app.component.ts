6155b2ea399dcd6e246d813bc589655d30325b6f
rahul <rahul.rsavant@gmail.com>
2020-11-28 22:10:52 +0530

    *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-<div [ngSwitch]="color">
-  <div *ngSwitchCase="'red'">you picked red color</div>
-  <div *ngSwitchCase="'blue'">you picked blue color</div>
+<div *ngFor="let color of colors">
+  {{color}}
 <div>
   `,
   styles: ['./app.component.css']
 })
 export class AppComponent {
 
-  public color="red";
-  title = 'codeevalution';
-  public isTrue=false;
+  public colors= ['red','green','blue'];
+  constructor(){}
 }
