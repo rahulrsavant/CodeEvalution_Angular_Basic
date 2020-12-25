36d67952f0b3ea32bb0655237ecbc2f1137c57dc
rahul <rahul.rsavant@gmail.com>
2020-12-25 15:28:14 +0530

    type predicate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +6,30 @@ import { Component } from '@angular/core';
   styles: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   public title="my_project_title";
   public name="vidula_infotech";
+
   constructor(){}
+  ngOnInit(): void {
+    function isString(s): s is string {
+  console.log("data type is",typeof(s))
+  return typeof s === 'string';
+}
+
+
+function toUpperCase(x: unknown) {
+  if(isString(x)) {
+    console.log(x.toUpperCase())
+  }else{
+    console.log("it's not a string")
+  }
+}
+
+console.log(toUpperCase(12));
+  }
+
+
+
 }
