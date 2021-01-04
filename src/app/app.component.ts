4642b54a96bea7afeea00d2937323c0df1293c99
rahul <rahul.rsavant@gmail.com>
2021-01-04 16:01:00 +0530

    os delete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Person } from './Person';
+import {Observable}from 'rxjs'
+
+
 @Component({
   selector: 'app-root',
   templateUrl:'./app.component.html' ,
@@ -7,29 +11,49 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class AppComponent implements OnInit {
-
-  public title="my_project_title";
-  public name="vidula_infotech";
-
   constructor(){}
   ngOnInit(): void {
-    function isString(s): s is string {
-  console.log("data type is",typeof(s))
-  return typeof s === 'string';
+
+/*     var d = [{'a': "something", 'b':12}, {'a': "something", 'b':12}, {'a': "somethingElse", 'b':12}, {'a': "something", 'b':12}, {'a': "somethingElse", 'b':12}]
+
+
+     function findLastIndex<T>(array: Array<T>, 
+      predicate: (value: T, index: number, obj: T[]) 
+      => boolean): number {
+        return 1;
+      }
+ 
+//=========================================================================================================
+findLastIndex(d, [{'a',12}]);
+findLastIndex(d, 'a', 'nothing');
+ */
+
+/* et someValue = [{'id': 1}, {'id': 2}, {'id': 3}, undefined, undefined];
+
+console.log(someValue)
+console.log(someValue.filter(val => val)); */
+
+function Observable(observer) {
+  //producer
+  let counter=1;                                                                          
+  const producer=setInterval(()=>{
+    observer.next(counter++);
+  },1000);
+
+  return ()=>{
+      clearInterval(producer);
+  };
 }
 
-
-function toUpperCase(x: unknown) {
-  if(isString(x)) {
-    console.log(x.toUpperCase())
-  }else{
-    console.log("it's not a string")
+const closeFn=Observable({
+    next:(data) => console.log(data),
+    error:(err)=>console.log('error',err),
+    complete:()=>console.log('done')    
+  });  
+  
+  setTimeout(()=>{
+    closeFn();
+  },5000);
+  
   }
-}
-
-console.log(toUpperCase(12));
-  }
-
-
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { EmployeeService } from './employee.service';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
 import { HttpClientModule} from '@angular/common/http';
+import { ProductComponent } from './product/product.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingComponents
+    routingComponents,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
